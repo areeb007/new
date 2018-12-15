763cd8cec5afa741238dae7a81deee6e8b89221c
areeb007 <45850253+areeb007@users.noreply.github.com>
2018-12-15 17:59:36 +0500

    new

---
+++ SignUp/SignUp/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SignUp.Models;
+
+namespace SignUp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Table u, HttpPostedFileBase file)
+        {
+            string path;
+            if (file != null)
+            {
+                try
+                {
+                    path = Path.Combine(Server.MapPath("~/Images"), path.GetFileName(file.FileName));
+                    file.SaveAs(path);
+                    u.Image = "/Images/" + path.getfileName(file.FileName);
+                    obj.Tables.Add(u);
+                    obj.Savechanges();
+
+                }
+                catch (Exception)
+                {
+
+                }
+
+            }
+            return View();
+
+
+        }
+}
+    
+}
